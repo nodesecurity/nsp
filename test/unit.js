d07b05be63817f36b011172eb32f136910372283
Caleb Hoover <choover11@gmail.com>
2015-10-12 16:25:42 -0700

    added nock to dev dependencies, added test for simple 404

--- test/unit.js
+++ test/unit.js
@@ -3,6 +3,7 @@
 var Code = require('code');
 var Lab = require('lab');
 var Check = require('../lib/check.js');
+var Nock = require('nock');
 
 var lab = exports.lab = Lab.script();
 var describe = lab.describe;
@@ -10,6 +11,15 @@ var it = lab.it;
 var expect = Code.expect;
 
 describe('check', function () {
+
+  it('Responds correctly when package.json can\'t be found', function (done) {
+
+    Check({ package: './package.json' }, function (err) {
+
+      expect(err.message).to.equal('Cannot find module \'./package.json\'');
+      done();
+    });
+  });
 
   it('Responds correctly when offline and can\' find a local advisories.json', function (done) {
 
@@ -25,6 +35,19 @@ describe('check', function () {
     Check({ package: '../package.json', shrinkwrap: './npm-shrinkwrap.json', offline: true }, function (err) {
 
       expect(err.message).to.equal('npm-shrinkwrap.json is required for offline mode');
+      done();
+    });
+  });
+
+  it('Responds correctly on 404', function (done) {
+
+    Nock('https://api.requiresafe.com')
+      .post('/check')
+      .reply(404);
+
+    Check({ package: '../package.json', shrinkwrap: './npm-shrinkwrap.json' }, function (err) {
+
+      expect(err.message).to.equal('Got an invalid response from requireSafe, please email the above debug output to support@requiresafe.com');
       done();
     });
   });
