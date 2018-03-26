bf8d92b8efe65667dd2a7e10631b9b5b6abce83d
Edgardo Avilés <eaviles@gmail.com>
2018-03-26 15:40:30 -0400

    add proxy to the gather command

--- commands/gather.js
+++ commands/gather.js
@@ -2,17 +2,18 @@
 
 const Fs = require('fs');
 const Path = require('path');
+const ProxyAgent = require('https-proxy-agent');
 
 const Command = require('../lib/command');
 const Wreck = require('../lib/wreck');
 
 const internals = {};
-internals.fetch = function (baseUrl, offset = 0, items = []) {
+internals.fetch = function (baseUrl, agent, offset = 0, items = []) {
 
-  return Wreck.get(`${baseUrl}/advisories?offset=${offset}`, { json: true }).then((res) => {
+  return Wreck.get(`/advisories?offset=${offset}`, { baseUrl, json: true, agent }).then((res) => {
 
     if (res.data.total > res.data.count + res.data.offset) {
-      return internals.fetch(baseUrl, res.data.offset + res.data.count, items.concat(res.data.results));
+      return internals.fetch(baseUrl, agent, res.data.offset + res.data.count, items.concat(res.data.results));
     }
 
     return items.concat(res.data.results);
@@ -33,7 +34,12 @@ exports.handler = Command.wrap('gather', (args) => {
 
   const path = args.path.endsWith('advisories.json') ? args.path : Path.join(args.path, 'advisories.json');
 
-  return internals.fetch(args.baseUrl).then((items) => {
+  let agent;
+  if (args.proxy) {
+    agent = new ProxyAgent(args.proxy);
+  }
+
+  return internals.fetch(args.baseUrl, agent).then((items) => {
 
     Fs.writeFileSync(path, JSON.stringify(items, null, 2), 'utf8');
     return {
